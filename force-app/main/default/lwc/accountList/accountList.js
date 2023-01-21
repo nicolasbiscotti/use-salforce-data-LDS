ff9b58012d4817e8124fee5ee38d256d456d0928
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-01-21 06:42:47 -0300

    Account list using apex class wired in LWC property

---
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -0,0 +1,21 @@
+import getAccounts from "@salesforce/apex/AccountController.getAccounts";
+import { LightningElement, wire } from "lwc";
+import Name from "@salesforce/schema/Account.Name";
+import AnnualRevenue from "@salesforce/schema/Account.AnnualRevenue";
+import Industry from "@salesforce/schema/Account.Industry";
+
+const COLUMNS = [
+  { label: "Account Name", fieldName: Name.fieldApiName, type: "text" },
+  {
+    label: "Anual Revenue",
+    fieldName: AnnualRevenue.fieldApiName,
+    type: "currency"
+  },
+  { label: "Industry", fieldName: Industry.fieldApiName, type: "text" }
+];
+
+export default class AccountList extends LightningElement {
+  columns = COLUMNS;
+  @wire(getAccounts)
+  accounts;
+}
