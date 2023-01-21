0522ded1db97b6805d9a1c246f745bfb051a14fe
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2023-01-21 17:19:16 -0300

    Handle Server Errors

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -3,6 +3,7 @@ import { LightningElement, wire } from "lwc";
 import Name from "@salesforce/schema/Account.Name";
 import AnnualRevenue from "@salesforce/schema/Account.AnnualRevenue";
 import Industry from "@salesforce/schema/Account.Industry";
+import { reduceErrors } from "c/ldsUtils";
 
 const COLUMNS = [
   { label: "Account Name", fieldName: Name.fieldApiName, type: "text" },
@@ -18,4 +19,7 @@ export default class AccountList extends LightningElement {
   columns = COLUMNS;
   @wire(getAccounts)
   accounts;
+  get errors() {
+    return this.accounts.error ? reduceErrors(this.accounts.error) : [];
+  }
 }
--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -3,6 +3,7 @@ import { LightningElement, wire } from "lwc";
 import FirstName from "@salesforce/schema/Contact.FirstName";
 import LastName from "@salesforce/schema/Contact.LastName";
 import Email from "@salesforce/schema/Contact.Email";
+import { reduceErrors } from "c/ldsUtils";
 
 const COLUMNS = [
   { label: "First Name", fieldName: FirstName.fieldApiName, type: "text" },
@@ -14,4 +15,7 @@ export default class ContactList extends LightningElement {
   columns = COLUMNS;
   @wire(getContacts)
   contacts;
+  get errors() {
+    return this.contacts.error ? reduceErrors(this.contacts.error) : [];
+  }
 }
